e941c0b5a970e10f88df1f96b8e5736d2c95adb1
Ravisharma-1999 <ravi829144@gmail.com>
2024-06-30 15:35:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+() => {
+    // Ensure Cypress waits for the element to appear before asserting
+    cy.get("th").first().should("contain", "Table-Header 1");
+    cy.get("th").last().should("contain", "Table-Header 2");
+}
